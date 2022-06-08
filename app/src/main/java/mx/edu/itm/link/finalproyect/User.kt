661af8d0ffca4bfebe3a888d70adb35a67f22e11
Francisco Jacinto <l17120242@morelia.tecnm.mx>
2022-06-08 07:10:57 -0500

    Cambios en MainActivity y ProfileFragment

---
+++ app/src/main/java/mx/edu/itm/link/finalproyect/User.kt
@@ -0,0 +1,8 @@
+package mx.edu.itm.link.finalproyect
+
+class User(name: String, level: String, date: String, item: Int) {
+    val name = name
+    val level = level
+    val date = date
+    val item = item
+}
