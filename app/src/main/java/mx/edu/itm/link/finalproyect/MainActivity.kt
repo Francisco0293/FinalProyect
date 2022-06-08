38220b99f8f45b8269eea94b693a820087b25a02
Francisco Jacinto <l17120242@morelia.tecnm.mx>
2022-06-08 07:01:47 -0500

    Cambios en MainActivity y ProfileFragment

--- app/src/main/java/mx/edu/itm/link/finalproyect/MainActivity.kt
+++ app/src/main/java/mx/edu/itm/link/finalproyect/MainActivity.kt
@@ -2,10 +2,13 @@ package mx.edu.itm.link.finalproyect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -17,5 +20,19 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         navView.setupWithNavController(navController)
+
+        //val database = Firebase.database
+        //val myRef = database.reference
+
+        //myRef.child("ejemplo_usuarios").child("usuario_1").setValue(User("francisco","Nivel_03","creado el 7 de mayo",3))
+        //myRef.child("ejemplo_usuarios").child("usuario_2").setValue(User("deni","Nivel 5","creado el 21 de abril del 2019",5))
+
+/*        myRef.child("ejemplo_usuarios").child("usuario_1").get().addOnSuccessListener { response ->
+            //TODO value
+            Log.d("firebaseResponse", response.value.toString())
+        }.addOnFailureListener {
+            //TODO
+            Log.e("firebaseResponse","Error getting data", it)
+        }*/
     }
 }
--- app/src/main/java/mx/edu/itm/link/finalproyect/ProfileFragment.kt
+++ app/src/main/java/mx/edu/itm/link/finalproyect/ProfileFragment.kt
@@ -1,18 +1,52 @@
 package mx.edu.itm.link.finalproyect
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
+import mx.edu.itm.link.finalproyect.databinding.FragmentProfileBinding
+import org.json.JSONObject
 
 class ProfileFragment : Fragment() {
-
+    private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container,false)
+
+        val database = Firebase.database
+
+
+        //myRef.child("ejemplo_usuarios").child("usuario_3").setValue(User("jonathan","Nivel 6","Creado el 2de agosto del 2016",10))
+
+
+        binding.btnGetUser.setOnClickListener {
+            val myRef = database.reference
+            val userArgument = binding.editTextUser.text.toString()
+            myRef.child("ejemplo_usuarios").child("$userArgument").get().addOnSuccessListener { response ->
+                val gson = Gson()
+                val json = gson.toJson(response.value)
+                val user: User = gson.fromJson(json, User::class.java)
+
+                binding.textView.text = user.name
+                binding.textView2.text = user.level
+                binding.textView3.text = user.date
+                binding.textView4.text = user.item.toString()
+
+                Log.d("firebaseResponse", response.value.toString())
+            }.addOnFailureListener {
+                Log.e("firebaseResponse","Error getting data", it)
+            }
+
+        }
+
+        return binding.root
     }
 }
